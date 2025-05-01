aa44f66e0a09c4c3889f551befcac8dd6ecc4ce2
ArturGevorgyan13 <138997601+ArturGevorgyan13@users.noreply.github.com>
2025-05-01 20:33:46 +0400

    Update main.cpp

--- AVL/main.cpp
+++ AVL/main.cpp
@@ -6,11 +6,13 @@ int main()
 
     AVL<int> tree;
 
-    tree.insert(tree.root,5);
-    tree.insert(tree.root,7);
-    tree.insert(tree.root,3);
-    tree.insert(tree.root,2);
-    tree.remove(tree.root,2);
+    tree.root=tree.insert(tree.root,5);
+    tree.root=tree.insert(tree.root,7);
+    tree.root=tree.insert(tree.root,3);
+    tree.root=tree.insert(tree.root,2);
+    tree.root=tree.insert(tree.root,1);
+    tree.root=tree.insert(tree.root,0);
+    tree.root=tree.insert(tree.root,10);
 
     std::cout<<tree.getheight(tree.root)<<std::endl;
 
